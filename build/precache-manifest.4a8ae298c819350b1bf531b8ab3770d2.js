01286343eee45f87ea6423e37d84313b5aca4d5a
Chuy <chuymedina96@gmail.com>
2021-03-09 21:28:27 -0600

    more

---
+++ build/precache-manifest.4a8ae298c819350b1bf531b8ab3770d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1459d732649018d588740aaf7ce7167b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bde5ea70cf629da2173",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "db47b762de15bb0165c7",
+    "url": "/static/js/2.7f19d5be.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.7f19d5be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bde5ea70cf629da2173",
+    "url": "/static/js/main.727e0a72.chunk.js"
+  },
+  {
+    "revision": "143a5b2d75734e6c1a49",
+    "url": "/static/js/runtime-main.7655c36f.js"
+  }
+]);
